9d395717564e038ca9146fe1f6a4d0b08e6ecbaf
Lucas Conte <lucasconte@MacBook-Pro-de-Lucas.local>
2018-10-31 20:49:04 -0300

    First commit

---
+++ app/src/main/java/com/prism/prismproject/MainDashboard.java
@@ -0,0 +1,13 @@
+package com.prism.prismproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard);
+    }
+}
